32a5f7e02c21732132fdb53f3a7d95adeaeade8a
Nazmul Hasan <shahajahan672@gmail.com>
2024-05-06 19:51:39 +0600

    files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyLibraryComponent } from '@nazmul_hasan/my-library';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MyLibraryComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
